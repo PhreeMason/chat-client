22145734aaeb3697c14238b8cefddb6a16ddd53a
Phreemason <github email address>
2017-07-01 22:39:14 -0400

    mapped out navigation links for site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-// import NavBar from './navigation/NavBar'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import NavBar from './Navs/NavBar'
+
+const home = () => (<div>home</div>)
+const signup = () => (<div>signup</div>)
+const dominoRoom = () =>(<div>dominoRoom</div>)
+const NotFound = () =>(<h1>Wrong turn</h1>)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <div className="App">
+          <div className="navbar">
+            <NavBar/>
+          </div>
+          <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/dominoRoom" component={dominoRoom} />
+          <Route component={NotFound}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
