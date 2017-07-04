becb7ccf9d3b8fa04bb69e9a52ce58042f7ad58f
Phreemason <github email address>
2017-07-04 02:00:04 -0400

    finished building out the signup form, need a way to passing the router prop

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'
+import { Form } from 'semantic-ui-react'
+import InputField from '../containers/Input'
 
-export default class SignUp extends React.Component {
-
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  render() {
-    return (
-      <div className="signup">
-      </div>
-    );
-  }
+const SignUpFormFunc=(props)=>{
+	  return (
+	    <div className="signup">
+	      <h2>Sign Up</h2>
+	      <Form onSubmit={data=>props.handleSubmit(data)}>	
+		  	  <Form.Field>
+			      <Field
+			      label='Username' 
+			      name="username" 
+			      component={InputField} 
+			      type="text" />
+			    </Form.Field>
+			    <Form.Field >
+			     <Field
+			     label='Email' 
+			      name="email" 
+			      component={InputField} 
+			      type="email" />
+			    </Form.Field>
+			    <Form.Field>
+			     <Field 
+			     label='Password'
+			      name="password" 
+			      component={InputField} 
+			      type="password" />
+			    </Form.Field>
+			    <Form.Button content='submit'/>
+			  </Form>
+	    </div>
+  );
 }
 
+
+const SignUpForm = reduxForm({
+	form: 'signup'
+})(SignUpFormFunc);
+
+export default SignUpForm;
