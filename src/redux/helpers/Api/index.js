7dfec024855a17a9d914522310695a8c67d7f4ca
Phreemason <github email address>
2017-07-04 03:38:48 -0400

    added image files for later use

--- src/redux/helpers/Api/index.js
+++ src/redux/helpers/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1';
+const url = 'http://localhost:3001/api/v1';
 
 // const userToken =()=> {
 // 	const token  = localStorage.getItem('token');
