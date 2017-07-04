ef6d455a86e335a14b0e45ea35cae586599302f3
Phreemason <github email address>
2017-07-04 02:00:34 -0400

    finished mapping out the login form also need to figure out how to pass in the router as an argument

---
+++ src/components/LogIn.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form'
+import { Form } from 'semantic-ui-react'
+import InputField from '../containers/Input'
+
+const LogInFormFunc =(props)=> {
+  return (
+    <div className="login">
+    <h2>Log In</h2>
+      <Form onSubmit={data=>props.handleSubmit(data)}>
+	  	  <Form.Field>
+		      <Field
+		      label='Username' 
+		      name="username" 
+		      component={InputField} 
+		      type="text" />
+		    </Form.Field>
+		    <Form.Field>
+		     <Field 
+		     label='Password'
+		      name="password" 
+		      component={InputField} 
+		      type="password" />
+		    </Form.Field>
+		    <Form.Button content='submit'/>
+		  </Form>
+    </div>
+  );
+}
+
+const LogInForm = reduxForm({
+	form: 'login'
+})(LogInFormFunc);
+
+export default LogInForm;
