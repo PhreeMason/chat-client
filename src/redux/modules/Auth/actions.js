c1953221d8f102704a7d1cf4879f30c9f19bc948
Phreemason <github email address>
2017-07-07 05:27:22 -0400

    added in an authentication wrapper for components

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -1,5 +1,7 @@
-import { reset, SubmissionError } from'redux-form'
+import { reset } from'redux-form'
 import serverApi from '../../helpers/Api'
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect'
+import Loading from '../../../components/Loading'
 
 export const authenticationRequest = () =>{
 	return {
@@ -14,21 +16,36 @@ export const setCurrentUser = user => {
 	}
 }
 
+export const authenticationFail = () =>{
+	return {
+		type: 'AUTHENTICATION_FAIL'
+	}
+}
+
+export const logout = (history) => {
+  localStorage.removeItem('token');
+  history.replace('/');
+  return { type: 'LOGOUT' };
+}
+
 
 export const signup = (user, history)=>{
 	return dispatch => {
 		dispatch(authenticationRequest())
 		return serverApi.createUser(user)
 		.then(body=>{
-	    const {user, token} = body.data
-			localStorage.setItem('token', token);
-			dispatch(setCurrentUser(user));
-			dispatch(reset('signup'));
-			history.push(`/dominoRoom`);
+	    if (body.errors) {
+	    	dispatch(authenticationFail)
+        history.push(`/`)      
+	    } else {
+		    const {user, token} = body.data
+				localStorage.setItem('token', token);
+				dispatch(setCurrentUser(user));
+				dispatch(reset('signup'));
+				history.push(`/dominoRoom`);
+	    }
 		})
-		.catch((err)=>{
-			throw new SubmissionError(err.message)
-		})
+	
 	}
 }
 
@@ -41,10 +58,37 @@ export const login = (user, history) => {
         localStorage.setItem('token', token);
         dispatch(setCurrentUser(user))
         dispatch(reset('login'));
-        history.push(`/dominoRoom`);
+        history.push(`/profile`);
       })
       .catch((err) => {
-        throw new SubmissionError(err.message)
+        dispatch(authenticationFail)
+        history.push(`/`)
       })
   }
 }
+
+export const authenticate = () => {
+  return dispatch => {
+    dispatch(authenticationRequest());
+    return serverApi.auth()
+      .then(response => {
+        const { user, token } = response;
+        localStorage.setItem('token', token);
+        dispatch(setCurrentUser(user));
+      })
+      .catch(err => {
+        localStorage.removeItem('token');
+        window.location = '/login';
+      });
+  };
+}
+
+export const userIsAuthenticated = connectedRouterRedirect({
+  redirectPath: '/login',
+  authenticatedSelector: state => state.auth.currentUser !== null,
+  wrapperDisplayName: 'UserIsAuthenticated',
+  // Returns true if the user auth state is loading
+  authenticatingSelector: state => state.auth.willAuthenticate,
+  // Render this component when the authenticatingSelector returns true
+  AuthenticatingComponent: Loading,
+})
