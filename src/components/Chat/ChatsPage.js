09b0fb070faa1f9cb90706bd4e305235ccc59778
Phreemason <github email address>
2017-07-09 01:22:42 -0400

    added currentChat state to keep track of most recent chat, also changed the dispatch function

--- src/components/Chat/ChatsPage.js
+++ src/components/Chat/ChatsPage.js
@@ -1,45 +1,50 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import * as chatActions from '../../redux/modules/Chats/actions'
+import {getChats} from '../../redux/modules/Chats/actions'
 import {Grid} from 'semantic-ui-react'
 import ChatList from './ChatList'
 import PropTypes from 'prop-types'
+import ChatShow from './ChatShow'
 
 class ChatPage extends React.Component {
-
+  
+  constructor(props) {
+    super(props);
+    this.state={
+      currentChat: null
+    }
+  }
 
   componentDidMount() {
-    console.log('i am here')
-    console.error(this.props)
     this.props.getChats()
+  }
+
+  handleClick=(chat)=>{
+    this.setState({
+      currentChat: chat
+    })
   }
 
   render() {
     return (
-      <Grid columns={3} padded>
+      <Grid columns={2} padded>
         <Grid.Column>
           <h1>Chats</h1>
-          <ChatList chats={this.props.chats}/>
+          <ChatList chats={this.props.chats} handleClick={this.handleClick} />
         </Grid.Column>
         <Grid.Column>
-          
-        </Grid.Column>
-        <Grid.Column>
-          
-        </Grid.Column>  
+          {this.state.currentChat !== null ? 
+            <ChatShow chat={this.state.currentChat}/>
+            : null}
+        </Grid.Column> 
       </Grid>
     );
   }
 }
 
 ChatPage.propTypes = {
-  chats: PropTypes.array.isRequired
-};
-
-const mapDispatchToProps = () => {
-  return {
-    getChats: chatActions.getChats
-  };
+  chats: PropTypes.array.isRequired,
+  getChats: PropTypes.func.isRequired,
 };
 
 
@@ -49,4 +54,4 @@ function mapStateToProps(state, ownProps) {
   }
 } 
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChatPage);  
+export default connect(mapStateToProps, {getChats})(ChatPage);  
