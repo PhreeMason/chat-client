96bc376173839e39275603c61403688dc0953a81
Phreemason <github email address>
2017-07-09 01:23:57 -0400

    removed styling from app and updated routes to be more restful

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Profile from '../components/Profile'
 import Auth from '../components/Auth'
-import { StyleSheet, css } from 'aphrodite';
+// import { StyleSheet, css } from 'aphrodite';
 import { userIsAuthenticated, authenticate, authenticationFail} from '../redux/modules/Auth/actions'
 import {connect} from 'react-redux'
+import ChatPage from '../components/Chat/ChatsPage'
 
-const styles = StyleSheet.create({
-    red: {
-        backgroundImage: 'linear-gradient( 135deg, #F05F57 0%, #360940 100%);'
-    }
-});
+// const styles = StyleSheet.create({
+//     red: {
+//         backgroundImage: 'linear-gradient( 135deg, #F05F57 0%, #360940 100%);'
+//     }
+// });
 
 const Games = () =>(<h1>Games</h1>)
 const NotFound = () =>(<h1>Wrong turn</h1>)
 const Home = ()=>(<h1>Home Page</h1>)
-const Chats=()=>(<h1>Chat Page</h1>)
 const HomePage = userIsAuthenticated(Home)
-const Chat = userIsAuthenticated(Chats)
+const Chats = userIsAuthenticated(ChatPage)
 const Pro= userIsAuthenticated(Profile)
 const Game = userIsAuthenticated(Games)
 
@@ -37,14 +37,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className={css(styles.red)}>
+        <div>
           <div className="navbar">
             <NavBar/>
           </div>
           <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={Auth} />
-          <Route exact path="/chat" component={Chat} />
+          <Route exact path="/chats" component={Chats} />
           <Route exact path="/profile" component={Pro} />
           <Route exact path="/games" component={Game} />
           <Route component={NotFound}/>
