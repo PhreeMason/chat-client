0f38e8abbc769255c6d0f3a2c98a2f2a146cb2f4
Phreemason <github email address>
2017-07-10 23:23:29 -0400

    fixed top level routes to allow for nested routes in other components. added error state to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Profile from '../components/Profile'
 import Auth from '../components/Auth'
+import {ErrorShow} from './ErrorShow'
 // import { StyleSheet, css } from 'aphrodite';
 import { userIsAuthenticated, authenticate, authenticationFail} from '../redux/modules/Auth/actions'
 import {connect} from 'react-redux'
-import ChatPage from '../components/Chat/ChatsPage'
-
+import ChatsPage from '../components/Chat'
 // const styles = StyleSheet.create({
 //     red: {
 //         backgroundImage: 'linear-gradient( 135deg, #F05F57 0%, #360940 100%);'
@@ -18,7 +18,6 @@ const Games = () =>(<h1>Games</h1>)
 const NotFound = () =>(<h1>Wrong turn</h1>)
 const Home = ()=>(<h1>Home Page</h1>)
 const HomePage = userIsAuthenticated(Home)
-const Chats = userIsAuthenticated(ChatPage)
 const Pro= userIsAuthenticated(Profile)
 const Game = userIsAuthenticated(Games)
 
@@ -28,26 +27,27 @@ class App extends Component {
   componentDidMount() {
     const token = localStorage.getItem('token');
     if (token) {
-      console.log('Fetching a new token!');
       this.props.authenticate();
     } else {
       this.props.authenticationFail();
     }
   }
   render() {
+    const {errors} = this.props
     return (
       <Router>
         <div>
           <div className="navbar">
             <NavBar/>
           </div>
+          {errors.length ? <ErrorShow errors={errors}/> : null}
           <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={Auth} />
-          <Route exact path="/chats" component={Chats} />
-          <Route exact path="/profile" component={Pro} />
-          <Route exact path="/games" component={Game} />
-          <Route component={NotFound}/>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/login" component={Auth} />
+            <Route path="/chats" component={userIsAuthenticated(ChatsPage)} />
+            <Route path="/profile" component={Pro} />
+            <Route path="/games" component={Game} />
+            <Route component={NotFound}/>
           </Switch>
         </div>
       </Router>
@@ -55,4 +55,10 @@ class App extends Component {
   }
 }
 
-export default connect(null, {userIsAuthenticated, authenticate, authenticationFail})(App);
+function mapStateToProps(state) {
+  return {
+    errors: state.errors.errors
+  }
+}
+
+export default connect(mapStateToProps, { userIsAuthenticated, authenticate, authenticationFail})(App);
