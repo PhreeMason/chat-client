5ace52aaa3c8e9208e1004eca99da088a20b9b2c
Phreemason <github email address>
2017-07-12 18:00:17 -0400

    created input firld to chat show

--- src/components/Chat/ChatInput.js
+++ src/components/Chat/ChatInput.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Form, Input, Button, Icon } from 'semantic-ui-react'
 
 export default class ChatInput extends React.Component {
+  constructor() {
+  	super();
+  	this.state={
+  		body: ''
+  	}
+  }
+  
+  handleChange = (event) =>{
+  	this.setState({
+  		body: event.target.value
+  	})
+  }
 
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    if (this.state.body) {
+    	this.props.handleSubmit(this.state.body)
+    	this.setState({
+    		body: ''
+    	})
+    }
+  }
 
   render() {
     return (
-      <div></div>
+      <Form onSubmit={this.handleSubmit}>
+		    <Input>
+		     <input onChange={this.handleChange} 
+		      value={this.state.body} />
+		      <Button icon type='submit'>
+            <Icon name='send outline'/>
+		      </Button>
+		    </Input>
+		  </Form>
     );
   }
 }
