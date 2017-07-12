5fcfa4eb20926a42fa23f2b7accb438c5cbcab73
Phreemason <github email address>
2017-07-12 17:57:26 -0400

    updated error render prop

--- src/containers/ErrorShow.js
+++ src/containers/ErrorShow.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Message } from 'semantic-ui-react'
 
 export const ErrorShow = (props) => (
-  <Message
-    color='orange'
-    header='Something went wrong'
-    list={props.errors}
-  />
+  <Message compact warning>
+    <Message.Header>Something went wrong!</Message.Header>
+    <ul>
+	    {props.errors.map((error, i) => <li key={i}>{error}</li>)}
+	  </ul>
+  </Message>
 )
 
