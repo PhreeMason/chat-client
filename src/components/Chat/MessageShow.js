0a240983deb004552e7d6811005d76c39163bf53
Phreemason <github email address>
2017-07-14 07:01:05 -0400

    messages will soon auto matcically scroll to the bottom

--- src/components/Chat/MessageShow.js
+++ src/components/Chat/MessageShow.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {List} from 'semantic-ui-react'
+import { StyleSheet, css } from 'aphrodite';
 
-export default class MessageShow extends React.Component {
+const sheet = StyleSheet.create({
+  messages:{
+    height: '300px',
+    overflowY: 'scroll'
+  }
+})
 
-  render() {
-    const {messages} = this.props
-    return (
+const MessageShow = ({messages}) =>{
+  // const messagesList = document.getElementByClassName('messages_5iovkz');
+  // setTimeout((messagesList) => {
+  //   messagesList.scrollTop = messagesList.scrollHeight;
+  // }, 1500)
+  return (
+    <div className={css(sheet.messages)}>
       <List>
-        {messages.map((message) => 
-           <List.Item key={message.id}>
+        {messages.map((message, i) => 
+           <List.Item key={i}>
               <List.Content>
                 <List.Header>{message.user_name}</List.Header>
                 <List.Description>{message.body}</List.Description>
@@ -16,7 +26,8 @@ export default class MessageShow extends React.Component {
            </List.Item>
         )}
       </List>
-    );
-  }
+    </div>
+  );
 }
 
+export default MessageShow
--- src/containers/ErrorShow.js
+++ src/containers/ErrorShow.js
@@ -9,4 +9,3 @@ export const ErrorShow = (props) => (
 	  </ul>
   </Message>
 )
-
