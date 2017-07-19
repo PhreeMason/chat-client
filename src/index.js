ff2ba371010fe85c4eb0c418a47aacdf247dd2d2
Phreemason <github email address>
2017-07-19 16:04:28 -0400

    added more styling to main app

--- src/index.js
+++ src/index.js
@@ -6,10 +6,20 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './containers/App';
 import store from './redux/store';
 import apiCable from './redux/helpers/ApiCable'
+import { StyleSheet, css  } from 'aphrodite';
+
+const styles = StyleSheet.create({
+    body: {
+        backgroundImage: 'linear-gradient( 135deg, #F05F57 0%, #360940 100%);',
+        minHeight: '900px'
+    }
+});
 
 ReactDOM.render(
 	<Provider store={store}>
-	  <App apiCable={apiCable}/>
+	  <div className={css(styles.body)}>
+	  	<App apiCable={apiCable}/>
+	  </div>
 	</Provider>, 
 	document.getElementById('root')
   );
