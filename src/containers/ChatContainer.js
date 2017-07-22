58f069148a5b5b38e37e87e2f3450e196b2ff180
Phreemason <github email address>
2017-07-22 06:16:06 -0400

    moved handling of web sockets connection to the chatshow component

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -6,35 +6,24 @@ import {Grid} from 'semantic-ui-react'
 import { Route, Switch } from 'react-router-dom';
 import ChatList from '../components/Chat/ChatList'
 import ChatShow from '../components/Chat/ChatShow'
-import {updateMesages} from '../redux/modules/Messages/actions' 
 import ChatsNew from '../components/Chat/ChatsNew'
 
 class ChatContainer extends React.Component {
 
   componentDidMount() {
     this.props.getChats()
-    if (!this.props.apiCable.messenger) {
-        this.props.apiCable.messenger = this.props.apiCable.subscriptions.create('ChatroomsChannel',{
-        connected: function() { console.log("cable: connected") },             // onConnect 
-        disconnected: function() { console.log("cable: disconnected") },       // onDisconnect 
-        received: (data) => this.props.updateMesages(data)        // OnReceive
-      }) 
-    } 
-  }
-  componentWillUnmount() {
-    this.props.apiCable.messenger.unsubscribe()
   }
 
   render() {
-    const { messages, chats, apiCable, match, username} = this.props
+    const { chats, apiCable, match, username} = this.props
     return (
     <Grid padded>
       <Grid.Column width={6}>
-        <ChatList chats={chats} handleClick={this.handleClick}/>
+        <ChatList chats={chats} username={username} handleClick={this.handleClick}/>
       </Grid.Column>
         <Switch>
           <Route path={`${match.url}/new`} component={ChatsNew} />
-          <Route path={`${match.url}/:chatId/:chatName`} render={(props)=> <ChatShow {...props} currentUsername={username} messages={messages}apiCable={apiCable}/> }/>
+          <Route path={`${match.url}/:chatId/:chatName`} render={(props)=> <ChatShow {...props} currentUsername={username} apiCable={apiCable}/> }/>
         </Switch>  
     </Grid>
     );
@@ -51,9 +40,8 @@ ChatContainer.propTypes = {
 function mapStateToProps(state) {
   return {
     chats: state.chats.chats,
-    messages: state.messages.messages,
-    username: state.auth.currentUser.username
+    username: state.auth.currentUser.username,
   }
 }
 
-export default connect(mapStateToProps, { getChats, updateMesages })(ChatContainer)
+export default connect(mapStateToProps, { getChats })(ChatContainer)
