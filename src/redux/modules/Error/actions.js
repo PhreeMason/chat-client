a87f9bb8f1ec799d489ede72c115f47149fbb186
Phreemason <github email address>
2017-07-22 06:25:13 -0400

    fixed spelling mistakes

--- src/redux/modules/Error/actions.js
+++ src/redux/modules/Error/actions.js
@@ -1,13 +1,15 @@
 const serverDown = () => ({type: 'NO_RESPONSE'})
 
 export const serverError = (errors) => {
-	return {
-		type: 'SERVER_ERROR',
-		errors
-	}
+  return {
+    type: 'SERVER_ERROR',
+    errors
+  }
 }
 
 const emptyErrors = ()=> ({type: 'NO_ERRORS'})
+
+const refreshPlease = () =>({type: 'TRY_AGAIN'})
 
 export const clearErrors =() =>{
   return dispatch =>{
@@ -16,18 +18,17 @@ export const clearErrors =() =>{
 }
 
 export const errorHandler = (error, dispatch) =>{
-	if (error.response) {
+  if (error.response) {
     const errors = logIT(error.response.data.errors)
     dispatch(serverError(errors));
   } else if (error.request) {
     dispatch(serverDown())
   } else {
-    alert('Something went wrong please rephresh and try again');
-  }
+    dispatch(refreshPlease())  }
 };
 
 function logIT(errors) {
-	var results = []
+  var results = []
   for (var k in errors){
     if (errors.hasOwnProperty(k)) {
         results = [...results, `${k}: ${errors[k]}`];
