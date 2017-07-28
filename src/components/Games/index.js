806db9ec48cb01ca165258d1e2ea0198f9f6e6e1
Phreemason <mr.omarmason@gmail.com>
2017-07-28 01:55:47 -0400

    added first domino to app

---
+++ src/components/Games/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Blank} from '../../images/dominos'
+
+const Games = () =>{
+  return (
+    <Blank/>
+  );
+}
+
+export default Games
---
+++ src/images/dominos/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Blank = () =>{
+	return (
+    <svg width="110" height="110" xmlns="http://www.w3.org/2000/svg" xmlnsSvg="http://www.w3.org/2000/svg">
+		  <g>
+			  <title>Layer 1</title>
+			  <rect className="domino_border" fill="none" strokeWidth="2" strokeDasharray="null" strokeLinejoin="null" strokeLinecap="null" x="32.5" y="9.60156" width="45" height="90" id="svg_12" stroke="#000000"/>
+			  <line className="domino_divider" id="svg_13" y2="55" x2="75" y1="55" x1="35" strokeLinecap="null" strokeLinejoin="null" strokeDasharray="null" strokeWidth="2" stroke="#000000" fill="none"/>
+		  </g>
+		</svg>
+	)
+}
