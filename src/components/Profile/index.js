d4aa551ca9a5ef0e97176a636ae087b0842fecd6
Phreemason <mr.omarmason@gmail.com>
2017-08-26 20:09:05 -0400

    working on getting current user and other user's profile to display on the page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,14 +3,26 @@ import {connect} from 'react-redux'
 import ProfileForm from './ProfileForm'
 import UserShow from './UserShow'
 import { Route } from 'react-router-dom'
+import {viewProfile} from '../../redux/modules/User/actions'
 
 class Profile extends React.Component {
+  
+  componentDidMount() {
 
+    console.log(this.props.match.params.username)
+  }
+  
+  componentWillMount() {
+    const {currentUser, match, viewProfile} = this.props
+    if (match.params.username !== currentUser.username) {
+      viewProfile(match.params.username)
+    }
+  }
   render() {
-  	const {currentUser, match} = this.props
+  	const {currentUser, match, currentProfile} = this.props
     return (
       <div>
-        <UserShow user={currentUser}/>
+        <UserShow user={currentProfile.username === currentUser.username ? currentUser : currentProfile}/>
         <Route path={`${match.url}/edit`} component={ProfileForm} />
       </div>
 		)
@@ -20,7 +32,8 @@ class Profile extends React.Component {
 const mapStateToProps=(state)=>{
   return {
     currentUser: state.auth.currentUser,
+    currentProfile: state.auth.currentProfile
   }
 }
 
-export default connect(mapStateToProps)(Profile)
+export default connect(mapStateToProps, {viewProfile})(Profile)
